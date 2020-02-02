17d1ee4a36742957bf7b9cc84c0dcbf6b2c65503
kboets <kurt.boets@telenetgroup.be>
2020-02-02 18:52:52 +0100

    update first component

---
+++ frontend/src/app/layout/service/date.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {GeneralError} from "../../general/generalError";
+import {catchError} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateService {
+
+  constructor(private http: HttpClient) { }
+
+  getCurrentTime() : Observable<Date | GeneralError> {
+      return this.http.get<Date>("/bst/api/current")
+          .pipe(
+              catchError(err => this.handleHttpError(err))
+          );
+  }
+
+  private handleHttpError(error: HttpErrorResponse) : Observable<GeneralError>{
+        let dataError = new GeneralError();
+        dataError.errorNumber = error.status;
+        dataError.errorMessage = error.message;
+        dataError.userFriendlyMessage = "Er liep iets fout bij het ophalen van de data";
+        return throwError(dataError);
+    }
+
+}
