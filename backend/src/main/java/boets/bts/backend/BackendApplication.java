1aae7a0fb1abacbdd71c199d5f2e52dd7b004879
kboets <kurt.boets@telenetgroup.be>
2021-01-27 17:17:46 +0100

    Initial deploy to dedicated Tomcat server

--- backend/src/main/java/boets/bts/backend/BackendApplication.java
+++ backend/src/main/java/boets/bts/backend/BackendApplication.java
@@ -5,12 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
